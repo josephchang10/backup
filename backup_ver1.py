2cceb5cba087e11634e3bbc16f02824219a5379e
张嘉夫 <zhangkuan523@icloud.com>
2016-12-05 13:11:45 +0800

    更新默认路径

--- backup_ver1.py
+++ backup_ver1.py
@@ -5,11 +5,11 @@ import os
 import time
 
 # 1. The files and directories to be backed up are specified in a list.
-source = ['/home/swaroop/byte', '/home/swaroop/bin']
+source = ['~/byte', '~/bin']
 # If you are using Windows, use source = [r'C:\Documents', r'D:\Work'] or something like that
 
 # 2. The backup must be stored in a main backup directory
-target_dir = '/mnt/e/backup/' # Remember to change this to what you will be using
+target_dir = '~/backup' # Remember to change this to what you will be using
 
 # 3. The files are backed up into a zip file.
 # 4. The name of the zip archive is the current date and time
